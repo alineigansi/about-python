45219fbe1cd3bc85815580269d09ecad2cc5be81
Aline Igansi Lazuli <59292894+alineigansi@users.noreply.github.com>
2020-05-07 16:01:46 -0300

    Add files via upload

---
+++ conversao-de-tipos-python.py
@@ -0,0 +1,10 @@
+Python 3.8.2 (tags/v3.8.2:7b3ab59, Feb 25 2020, 23:03:10) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> x=9
+>>> y=7.5
+>>> z=(x+y)/4
+>>> print('Iniciando o sistema....')
+Iniciando o sistema....
+>>> print('O valor inicial da variável é: '+str(z))
+O valor inicial da variável é: 4.125
+>>> 
